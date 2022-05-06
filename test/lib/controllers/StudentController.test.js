93f34c366ec3f88287c0af7e734adb086f2de83d
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-06 13:50:05 -0500

    Paso 23: Modificando test para probar GitHub Action

--- test/lib/controllers/StudentController.test.js
+++ test/lib/controllers/StudentController.test.js
@@ -5,7 +5,7 @@ describe("Unit Test for StudentController class", () => {
 
     test("Test 1.- Get all students", () => {
 
-        const students = StudentController.getAllStudents();
+        const studentsa = StudentController.getAllStudents();
         expect(students).not.toHaveLength(0);
 
     });
